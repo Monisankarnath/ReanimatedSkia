787c075a2014384e1b577d3bc1e2020a65e0803c
Monisankar Nath <monisankar.nath161998@gmail.com>
2023-03-18 23:01:54 +0530

    volume part 3 not going as expected

--- src/Music.js
+++ src/Music.js
@@ -1,62 +1,36 @@
 import React from 'react';
-import {Box, HStack} from 'native-base';
+import {Box} from 'native-base';
 import Volume from './components/Volume';
-import Animated, {
-  Extrapolation,
-  interpolate,
-  useAnimatedProps,
-  useAnimatedStyle,
-  useSharedValue,
-  withSpring,
-  withTiming,
-} from 'react-native-reanimated';
+import {useSharedValue, withSpring} from 'react-native-reanimated';
 import {
   Gesture,
   GestureDetector,
   GestureHandlerRootView,
 } from 'react-native-gesture-handler';
 
-const AnimatedHStack = Animated.createAnimatedComponent(HStack);
-const AnimatedBox = Animated.createAnimatedComponent(Box);
-
+const springConfig = {
+  damping: 5,
+  mass: 0.1,
+  stiffness: 50,
+};
 const Music = () => {
   const volBarW = useSharedValue(0);
   const startVolBarW = useSharedValue(0);
   const volBarPressed = useSharedValue(false);
-  const volBarOuterW = useSharedValue(160);
-  const animatedBar = useAnimatedProps(() => ({
-    width: `${interpolate(volBarW.value, [0, volBarOuterW.value], [0, 100], {
-      extrapolateLeft: Extrapolation.CLAMP,
-      extrapolateRight: Extrapolation.CLAMP,
-    })}%`,
-  }));
-  const animatedOuterBar = useAnimatedStyle(() => {
-    const springConfig = {
-      damping: 5,
-      mass: 0.1,
-      stiffness: 50,
-    };
-    return {
-      width: volBarPressed.value
-        ? withSpring(220, springConfig)
-        : withSpring(160, springConfig),
-      height: volBarPressed.value
-        ? withSpring(40, springConfig)
-        : withSpring(8, springConfig),
-    };
-  });
+  const volBarOuterW = useSharedValue(volBarPressed ? 220 : 140);
+
   const gesture = Gesture.Pan()
-    .onBegin(e => {
+    .onBegin(() => {
       volBarPressed.value = true;
     })
     .onUpdate(e => {
       volBarW.value = e.translationX + startVolBarW.value;
     })
-    .onEnd(e => {
+    .onEnd(() => {
       if (volBarW.value < 0) {
         volBarW.value = 0;
-      } else if (volBarW.value > 160) {
-        volBarW.value = 160;
+      } else if (volBarW.value > 140) {
+        volBarW.value = 140;
       }
       startVolBarW.value = volBarW.value;
     })
@@ -72,29 +46,15 @@ const Music = () => {
           justifyContent="center"
           height={40}>
           <GestureDetector gesture={gesture}>
-            <AnimatedHStack
-              space={2}
-              position="relative"
-              alignItems="center"
-              w="220px"
-              h="50px">
-              <Box position="absolute" left={4} zIndex={999}>
-                <Volume h={50} w={40} volBarW={volBarW} color={'white'} />
-              </Box>
-              <AnimatedBox
-                bg="#343434"
-                style={animatedOuterBar}
-                position="absolute"
-                right={0}
-                borderRadius={8}>
-                <AnimatedBox
-                  bg="white"
-                  animatedProps={animatedBar}
-                  height="100%"
-                  borderRadius={8}
-                />
-              </AnimatedBox>
-            </AnimatedHStack>
+            <Volume
+              h={50}
+              w={40}
+              volBarW={volBarW}
+              volBarPressed={volBarPressed}
+              springConfig={springConfig}
+              volBarOuterW={volBarOuterW}
+              color={'white'}
+            />
           </GestureDetector>
         </Box>
       </Box>
--- src/components/AnimatedVolume.js
+++ src/components/AnimatedVolume.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Animated, {useAnimatedProps, withTiming} from 'react-native-reanimated';
+import Animated, {withTiming, useAnimatedProps} from 'react-native-reanimated';
 import {Path} from 'react-native-svg';
 
 const AnimatedPath = Animated.createAnimatedComponent(Path);
